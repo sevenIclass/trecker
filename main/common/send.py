25189e94f9b57387822de0a5f738461bbfea2744
sevenIclass <158577932+sevenIclass@users.noreply.github.com>
2024-02-02 16:16:27 +0300

    Update send.py

--- main/common/send.py
+++ main/common/send.py
@@ -1,5 +1,5 @@
 import requests
 token = '6495698589:AAF8IrqRoPuEERHNFztOqnikDtxL_xDBU10'
 def send(file):
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={5270716903}&text={'тест'}"
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={5270716903}&text={'тест'}" #отправляет мне сообщение 'тест'
     print(requests.get(url).json())
