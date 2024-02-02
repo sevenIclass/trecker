b6aa0fb050bcdb0951b996db6666f59c4abb0624
sevenIclass <158577932+sevenIclass@users.noreply.github.com>
2024-02-02 15:54:34 +0300

    Update menu.py

--- main/common/files/menu.py
+++ main/common/files/menu.py
@@ -3,7 +3,5 @@ from aiogram.types import BotCommand
 private = [
 ]
 
-admin = [
-    BotCommand(command="nah, i'd win", description='-gojo')
-]
-#BotCommand(command='info', description='ya')
+
+#BotCommand(command='command', description='description')
